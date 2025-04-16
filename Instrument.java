4dfd5a41af63cd47634f1c12e89c7172e71fb983
Shannon Sos <ssos@pleasantonusd.net>
2025-04-16 10:32:47 -0700

    Update Instrument.java

--- Instrument.java
+++ Instrument.java
@@ -1,6 +1,9 @@
 public abstract class Instrument {
+
+  //method will "play" so it should do something in your code
   public abstract void play();
 
+  //method will "tune" so it should do something in your code that is different from tune
   public abstract void tune();
 
   public void done()
