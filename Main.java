a0f1a4d5aac19d61674f589fd4d66d056e2acb1f
Shannon Sos <ssos@pleasantonusd.net>
2025-04-16 10:29:27 -0700

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,22 @@
+public class Main {
+  public static void main(String[] args) {
+    //Instrument instrument = new Instrument(); //DOES THIS WORK? -> NO
+
+    //Violin violin = new Violin();
+
+    //Glockenspiel glockenspiel = new Glockenspiel();
+
+    //instrument.tune();
+    //violin.tune();
+    //glockenspiel.tune();
+
+    //instrument.play();
+    //violin.play();
+    //glockenspiel.play();
+
+    //instrument.done();
+    violin.done();
+    //glockenspiel.done();
+  }
+
+}
