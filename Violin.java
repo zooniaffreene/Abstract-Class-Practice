097accf5da82ef9e2443fd5eecb456922efec569
zooniaffreene <zooniaffreene1073@gmail.com>
2025-04-21 23:49:51 -0700

    Update Violin.java

--- Violin.java
+++ Violin.java
@@ -1,3 +1,10 @@
 public class Violin extends Instrument {
 
+  public void play() {
+    System.out.println("The violin is playing a beautiful melody.");
+  }
+
+  public void tune() {
+    System.out.println("Tuning the violin carefully...");
+  }
 }
