876099b83f5bb4149b0ae5d5edbdb54ccd05dad9
zooniaffreene <zooniaffreene1073@gmail.com>
2025-04-21 23:50:10 -0700

    Update Glockenspiel.java

--- Glockenspiel.java
+++ Glockenspiel.java
@@ -1,3 +1,10 @@
 public class Glockenspiel extends Instrument {
 
+  public void play() {
+    System.out.println("The glockenspiel is playing cheerful notes.");
+  }
+
+  public void tune() {
+    System.out.println("Tuning the glockenspiel with a tuning fork...");
+  }
 }
